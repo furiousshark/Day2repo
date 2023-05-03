3fda195ab0fa97f1ed2aee3e977bc5d622a53c2d
furiousshark <nikamkuldeep131@gmail.com>
2023-05-03 13:05:19 +0530

    udate success

---
+++ Admission_CRUD/src/main/java/com/sppu/AdmissionCrudApplication.java
@@ -0,0 +1,13 @@
+package com.sppu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmissionCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdmissionCrudApplication.class, args);
+	}
+
+}
